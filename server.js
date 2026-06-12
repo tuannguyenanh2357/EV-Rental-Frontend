83ee4e15b5798bb9f16e4e268ef0887e6f04b341
Nguyễn Anh Tuấn <tuannguyenanh2357@gmail.com>
2026-06-13 00:41:02 +0700

    bind port to 0.0.0.0

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(function(req, res) {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
