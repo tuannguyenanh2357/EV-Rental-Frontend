2bf5452433f8750b355e6d13e2a785ac4a2deed4
Nguyễn Anh Tuấn <tuannguyenanh2357@gmail.com>
2026-06-13 00:13:11 +0700

    deploy_railway

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080';
+//export const API_BASE_URL = 'http://localhost:8080';
 
+export const API_BASE_URL = 'https://ev-rental-backend-production.up.railway.app';
